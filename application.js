624a24770050189209d3859698a9ef179201705c
Adeyemo <davidadeyemo12@gmail.com>
2016-08-16 23:05:24 -1200

    Update application.js

--- application.js
+++ application.js
@@ -1,14 +1,14 @@
 class NotesApplication(){
-  let noteList = [];
-  constructor (autor) {
-    //validate
-    this.autor = autor;
+  let notelist = [];
+  constructor (author) {
+    //validate autor
+    this.author = author;
 
   /**
    * function to take note content and add to notelist object
   */
   create(note_content){
-    noteList.push(note_content);
+    notelist.push(note_content);
   }
 
 
@@ -20,11 +20,57 @@ class NotesApplication(){
       console.log('Note Id :' + i );
       console.log(notelist[i]);
     }
-    consol.log('By Autho :' + this.autor);
+    consol.log('By Author :' + this.author);
 
   }
 
   /**
+  *This function takes the note id and return the note content as string
+  */
+  get(note_id){
+    if(Math.abs(note_id) < notelist.length ){
+      return notelist[note_id];
+    }else{
+      return "Note Id is Not Valid";
+    }
+  }
+
+
+/**
+*This function takes in a seach text and returns all note with that text within in
+*/
+
+  search(search_text){
+
+  }
+
+
+/**
+*This function delete the note from notelist that has its index passed
+*/
+
+  delete(note_id){
+    if(Math.abs(note_id) < notelist.length ){
+      delete notelist[note_id];
+    }else{
+      return "Note Id is Not Valid";
+    }
+  }
+
+
+/**
+*This function replace the content of note with new one at note_id
+*/
+  edit(note_id, new_content){
+    if(Math.abs(note_id) < notelist.length  && new_content != ""){
+      notelist[note_id] = new_content;
+    }else{
+      return "Note Id is Not Valid";
+    }
+  }
+
+
+}
   *This function takes the note id and return the note content as string
   */
   get(note_id){
