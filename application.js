81a13cb2acbc19cd4428a71f171341b2b9e7c427
ADEYEMO <ADEYEMO>
2016-08-17 14:58:47 -1200

    Updated  Searh text Method

--- application.js
+++ application.js
@@ -1,14 +1,17 @@
-class NotesApplication(){
-  let notelist = [];
+class NotesApplication{
+  //var notelist = [];
+
   constructor (author) {
     //validate autor
     this.author = author;
+     this.notelist = [];
   }
   /**
    * function to take note content and add to notelist object
   */
   create(note_content){
-    notelist.push(note_content);
+    this.notelist.push(note_content);
+    //return this.notelist;
   }
 
 
@@ -16,9 +19,9 @@ class NotesApplication(){
   * This function list out note id, note and authors
   */
   listNote(){
-    for(var i =0; i < notelist.length; i++ ){
+    for(var i =0; i < this.notelist.length; i++ ){
       console.log('Note Id :' + i );
-      console.log(notelist[i]);
+      console.log(this.notelist[i]);
     }
     console.log('By Author :' + this.author);
 
@@ -28,8 +31,8 @@ class NotesApplication(){
   *This function takes the note id and return the note content as string
   */
   getNoteId(note_id){
-    if(Math.abs(note_id) < notelist.length ){
-      return notelist[note_id];
+    if(Math.abs(note_id) < this.notelist.length ){
+      return this.notelist[note_id];
     }else{
       return "Note Id is Not Valid";
     }
@@ -41,10 +44,11 @@ class NotesApplication(){
 */
 
   searchText(search_text){
-    for(var i =0; i < notelist.length; i++ ){
+    for(var i =0; i < this.notelist.length; i++ ){
       if(notelist.indexOf("search_text") >= 0){
+        console.log('Showing result for search :' + '['+search_text+']' );
         console.log('Note Id :' + i );
-        console.log(notelist[i]);
+        console.log(this.notelist[i]);
         console.log('By Author :' + this.author);
       }
 
@@ -58,10 +62,10 @@ class NotesApplication(){
 */
 
   deleteNote(note_id){
-    if(Math.abs(note_id) < notelist.length ){
-      delete notelist[note_id];
+    if(Math.abs(note_id) < this.notelist.length ){
+      delete this.notelist[note_id];
     }else{
-      return "Note Id is Not Valid";
+      return "Note Id is Not Valid Please Enter a valid Note Id";
     }
   }
 
@@ -70,10 +74,10 @@ class NotesApplication(){
 *This function replace the content of note with new one at note_id
 */
   editNote(note_id, new_content){
-    if(Math.abs(note_id) < notelist.length  && new_content != ""){
-      notelist[note_id] = new_content;
+    if(Math.abs(note_id) < this.notelist.length  && new_content !== ""){
+      this.notelist[note_id] = new_content;
     }else{
-      return "Note Id is Not Valid";
+      return "Note Id is Not Valid Please Enter a valid Note Id and Content Must not be Empty";
     }
   }
 
